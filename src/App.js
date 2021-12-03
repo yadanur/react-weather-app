5e58b23a13204f92db2aafbbf39ca3395302ea40
yadanur <y.adanur@gmail.com>
2021-12-03 00:00:49 -0800

    .

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           <input
           type="text"
           className="search-bar"
-          placeholder="Search.."
+          placeholder="Search..."
           onChange={e => setQuery(e.target.value)}
           value={query}
           onKeyPress={search}
